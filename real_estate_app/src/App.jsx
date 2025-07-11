05b03d7f0a5b3f0b9a41755716e8327993220c55
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2025-07-11 23:21:03 +0600

    Change in App.jsx code

--- real_estate_app/src/App.jsx
+++ real_estate_app/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
+import HomePage from './pages/HomePage'
 
 
 
@@ -11,6 +12,11 @@ function App() {
     <Router>
       <div className='min-h-screen bg-gray-50'>
          <Header />
+         <main>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+         </main>
          <Footer />
       </div>
     </Router>
